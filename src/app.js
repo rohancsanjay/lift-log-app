183d798985a2dafb3cbe9c7b2f7ae232a48018c7
Rohan Shitole <rohancsanjay@gmail.com>
2017-10-04 15:41:32 -0700

    Removed Dummy Data from src/app.js

--- src/app.js
+++ src/app.js
@@ -11,15 +11,6 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addRoutine({ exercise: 'Leg', weight: 135, sets: 3, reps: 10 }));
-store.dispatch(addRoutine({ exercise: 'Chest', weight: 140, createdAt: 1000, sets: 3, reps: 10 }));
-store.dispatch(addRoutine({ exercise: 'Bicep', weight: 85, createdAt: 100, sets: 3, reps: 10 }));
-// store.dispatch(addRoutine({ title: 'Shoulder', weight: 95, createdAt: 250 }));
-// store.dispatch(addRoutine({ title: 'Back', weight: 130 }));
-
-const state = store.getState();
-const visibileRoutines = getVisibleRoutines(state.routines, state.filters);
-
 console.log(visibileRoutines);
 
 const jsx = (
